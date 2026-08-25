b2f47fd7ce4048227c3116a49df470bf232d1e35
yogibytes <getupwithyogu@gmail.com>
2026-08-25 23:01:00 +0530

    fix the cors issue by updating the vite.config.ts used the proxy routes :server:{proxy:{'/api':{target:'http://localhost:3000',changeOrigin:true,}}}

--- packages/widget/vite.config.ts
+++ packages/widget/vite.config.ts
@@ -20,9 +20,34 @@ export default defineConfig({
       },
     },
   },
+server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
 })
+// export default {
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000',
+//         changeOrigin: true,
+//       }
+//     }
+//   }
+// }
+  // server: {
+  //   cors: {
+  //     origin: 'http://localhost:3000',
+  //     credentials: true,
+  //     preflightContinue:false,
+  //   }
+  // },
