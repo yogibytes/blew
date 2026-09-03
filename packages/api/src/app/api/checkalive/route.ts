21af3c6fcbcf6aed5b34e688f9eb75752ad6df08
yogibytes <getupwithyogu@gmail.com>
2026-09-04 00:06:15 +0530

    fix the route for correct wallet address

--- packages/api/src/app/api/checkalive/route.ts
+++ packages/api/src/app/api/checkalive/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-    // This will print in your terminal shell when Postman hits the URL
+    // This will print in your terminal she..
+    // ll when Postman hits the URL
     console.log("chilll server is alive"); 
 
     return NextResponse.json({
